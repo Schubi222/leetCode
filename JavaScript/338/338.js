30d926ec5a02743db534af776f5551d00ff1e149
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-09-01 09:03:01 +0200

    readded solution

--- JavaScript/338/338.js
+++ JavaScript/338/338.js
@@ -3,6 +3,20 @@
  * @return {number[]}
  */
 var countBits = function(n) {
+    let ans = []
+    for (let i = 0; i < n+1; i++) {
+        let num = i
+        let ones = 0
+        while (num > 0){
+            if(num % 2 === 1){
+                ones++
+            }
+            num = Math.floor(num/2)
+        }
+        ans.push(ones)
+    }
+
+    return ans
 };
 
 
